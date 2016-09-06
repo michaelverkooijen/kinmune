ca9b6b50152dba4e168a6def1167c1072f707b3f
Michael Verkooijen <michaelverkooijen@gmail.com>
2016-09-06 19:12:06 +0200

    removed newlines from content

--- relay_discussions.py
+++ relay_discussions.py
@@ -34,5 +34,5 @@ for post in reversed(r.json()['_embedded']['doc:posts']):
         name = post['createdBy']['name']
         forum_name = post['forumName']
         thread_id = post['threadId']
-        message = "<https://tes.wikia.com/d/p/" + thread_id + "|" + content + "> —  *<http://tes.wikia.com/wiki/User:" + name + "|" + name + ">* in _" + forum_name + "_"
+        message = "<https://tes.wikia.com/d/p/" + thread_id + "|" + content.replace('\n', ' ') + "> —  *<http://tes.wikia.com/wiki/User:" + name + "|" + name + ">* in _" + forum_name + "_"
         slack_client.api_call("chat.postMessage", channel="discussions-rc", text=message, as_user=True)
